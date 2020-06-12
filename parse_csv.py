236ca6ef03b06d3e2275d1d502e4dd2cc751e6d8
meysam hashemi <meysam.hashemi@gmail.com>
2020-06-12 18:17:28 +0200

    Add files via upload

---
+++ parse_csv.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+def parse_csv(fname):
+    lines = []
+    with open(fname, 'r') as fd:
+        for line in fd.readlines():
+            if not line.startswith('#'):
+                lines.append(line.strip().split(','))
+    names = [field.split('.') for field in lines[0]]
+    data = np.array([[float(f) for f in line] for line in lines[1:]])
+
+    namemap = {}
+    maxdims = {}
+    for i, name in enumerate(names):
+        if name[0] not in namemap:
+            namemap[name[0]] = []
+        namemap[name[0]].append(i)
+        if len(name) > 1:
+            maxdims[name[0]] = name[1:]
+
+    for name in maxdims.keys():
+        dims = []
+        for dim in maxdims[name]:
+            dims.append(int(dim))
+        maxdims[name] = tuple(reversed(dims))
+
+    # data in linear order per Stan, e.g. mat is col maj
+    data_ = {}
+    for name, idx in namemap.items():
+        new_shape = (-1, ) + maxdims.get(name, ())
+        data_[name] = data[:, idx].reshape(new_shape)
+
+    return data_
