9c175eed8c018e2a2bc9f0db85ad8733c9288937
meysam hashemi <meysam.hashemi@gmail.com>
2020-12-21 11:00:36 +0100

    Delete ComputeCV.sh

--- ComputeCV.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-model=Epileptor2D_Hypos
-alg=hmc
-
-python ComputeWAIC_Hypos.py data_input2D    ${model}  data_output_CV_${alg}_${model}
-
-python ComputePSIS_Hypos.py data_input2D  ${model}  data_output_CV_${alg}_${model}
